28a7b839fd8b6dd5f2ae7d487023d54763440dbb
João Vitor Marques Braga <102833436+Joao-Vitor-Marques-Braga@users.noreply.github.com>
2023-01-13 11:34:05 -0300

    Update index.jsx

--- src/components/StartScreen/index.jsx
+++ src/components/StartScreen/index.jsx
@@ -3,7 +3,7 @@ import './style.css'
 const GameStart = ({ startGame }) => {
   return (
     <div className="start">
-      <h1>Secret Word</h1>
+      <h1>Vocábulo</h1>
       <p>Clique no botão abaixo para começar a jogar</p>
       <button onClick={startGame}>Começar jogo</button>
     </div>
