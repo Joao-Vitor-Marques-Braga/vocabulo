64ad68529dca46b5b0c320197293280dd1c99f95
João Vitor Marques Braga <102833436+Joao-Vitor-Marques-Braga@users.noreply.github.com>
2023-01-13 11:31:14 -0300

    Add files via upload

---
+++ src/data/word.js
@@ -0,0 +1,7 @@
+export const wordsList = {
+    carro: ['Motor', 'Volante', 'Roda', 'Direção'],
+    fruta: ['Banana', 'Maça', 'Mamão', 'Abacate', 'Abacaxi', "Pera"],
+    corpo: ['Braço', 'Perna', 'Coração', 'Musculo', 'Biceps', 'Triceps', 'Peito'],
+    computador: ['Mouse', 'Teclado', 'Placa Mãe', 'Placa de video', 'Gabinete'],
+    esporte: ['Futebol', 'Vôlei', 'Tênis']
+}
